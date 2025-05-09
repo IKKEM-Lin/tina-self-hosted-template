2f3fcaa97c8b2261e1a8b527d9951a01d3db6c81
Mark <mark@xmu.edu.cn>
2025-05-09 16:26:02 +0800

    build: fix path issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isSSG = process.env.TINA_PUBLIC_ENV === "ssg";
+const SSG_BASE_PATH = process.env.SSG_BASE_PATH || "";
 
 const nextConfig = isSSG
   ? {
       output: "export",
+      basePath: SSG_BASE_PATH,
       images: {
         loader: "custom",
         loaderFile: "./share/ssg-image-loader.ts",
