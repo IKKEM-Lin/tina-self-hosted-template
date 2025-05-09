2c85fcd0a3010dc487602a42614811fa71827fb8
Mark <mark@xmu.edu.cn>
2025-05-09 16:48:27 +0800

    build: fix base path image issue

--- share/ssg-image-loader.ts
+++ share/ssg-image-loader.ts
@@ -1,6 +1,8 @@
 'use client'
  
+const SSG_BASE_PATH = process.env.SSG_BASE_PATH || "";
+
 export default function ssgImageLoader({ src }: any) {
   // console.log(src, width, quality)
-  return `${src.replace(/^\/api\/images/,"/uploads")}`
+  return `${SSG_BASE_PATH}${src.replace(/^\/api\/images/,"/uploads")}`
 }
